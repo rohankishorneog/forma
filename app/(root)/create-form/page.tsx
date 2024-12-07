3c84f154c70fbe289182a0b2c877101e7f6dda57
rohankishorneog <rkn2601@gmail.com>
2024-12-07 23:00:55 +0530

    implemented the functionality of adding and removing a field to the form canvas

--- app/(root)/create-form/page.tsx
+++ app/(root)/create-form/page.tsx
@@ -1,11 +1,40 @@
+"use client";
+
 import FormCanvas from "@/app/components/FormCanvas/FormCanvas";
-import React from "react";
+import { fields } from "@/app/constants/constants";
+import { Field_Types } from "@/app/types";
+import React, { useState } from "react";
 
 const Page = () => {
+  const [formFields, setFormFields] = useState<Field_Types[]>([]);
+
+  const handleAddField = (newField: Field_Types) => {
+    setFormFields((prevFields) => [
+      ...prevFields,
+      { ...newField, id: Date.now().toLocaleString() },
+    ]);
+  };
+
+  const removeFormField = (field: Field_Types) => {
+    setFormFields((prevFields) =>
+      prevFields.filter((prevField) => prevField.id !== field.id)
+    );
+  };
+
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
-     
-      <FormCanvas />
+    <div className="flex flex-col w-screen h-screen items-center pt-12">
+      <div className="w-full justify-center gap-7 mb-10 hidden md:flex">
+        {fields.map((field) => (
+          <button
+            key={field.label}
+            className="bg-slate-900 text-white px-2 py-1 rounded-md"
+            onClick={() => handleAddField(field)}
+          >
+            {field.type}
+          </button>
+        ))}
+      </div>
+      <FormCanvas fields={formFields} handleRemove={removeFormField} />
     </div>
   );
 };
--- app/components/FormCanvas/FormCanvas.tsx
+++ app/components/FormCanvas/FormCanvas.tsx
@@ -1,15 +1,62 @@
 import React from "react";
 import MobileAddFieldButton from "../MobileAddFieldButton/MobileAddFieldButton";
+import { Field_Types } from "@/app/types";
 
-const FormCanvas = () => {
+interface FormCanvasProps {
+  fields: Field_Types[];
+  handleRemove: (field: Field_Types) => void;
+}
+
+const FormCanvas = ({ fields, handleRemove }: FormCanvasProps) => {
   return (
     <>
       <MobileAddFieldButton />
-      <div className="bg-slate-600 border-2 border-black shadow-md h-[calc(100vh-10rem)] flex items-center rounded-md p-10">
-        Add fields by clicking or dragging the fields here.
+      <div
+        className={`bg-slate-600 border-2 border-black shadow-md h-[calc(100vh-10rem)] flex  ${
+          fields.length > 0 ? "items-start" : "items-center"
+        } rounded-md p-10 sm:w-full md:w-[500px] lg:w-[750px] overflow-y-auto`}
+      >
+        {fields.length === 0 && (
+          <p>Add fields by clicking or dragging the fields here.</p>
+        )}
+
+        <div className="flex flex-col gap-2 w-full">
+          {fields.map((field: Field_Types) => (
+            <div key={field.id} className="flex gap-2">
+              <div className="flex-1">
+                <label>{field.label}</label>
+                {getFieldComponent(field)}
+              </div>
+
+              <div className="self-end flex flex-col gap-2">
+                <button>E</button>
+                <button onClick={() => handleRemove(field)}>R</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
 };
 
 export default FormCanvas;
+
+const getFieldComponent = (field: Field_Types) => {
+  switch (field.type.toLowerCase()) {
+    case "short answer":
+    case "long answer":
+      return <textarea className="w-full" placeholder="Enter your answer" />;
+    case "select":
+      return (
+        <select className="w-full">
+          {field.options &&
+            field.options.map((option, index) => (
+              <option key={index}>{option}</option>
+            ))}
+        </select>
+      );
+    default:
+      return <input type={field.type} className="w-full" />;
+  }
+};
