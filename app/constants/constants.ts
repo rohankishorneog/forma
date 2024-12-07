26bc9226c535db9c4962e085044c7b5069facbd5
rohankishorneog <rkn2601@gmail.com>
2024-12-07 21:07:10 +0530

    Created constants for field types of the form

---
+++ app/constants/constants.ts
@@ -0,0 +1,8 @@
+export const Field_Types=[
+    {type:'short answer', label:'Short Answer'},
+    {type:'long answer', label:'Short Answer'},
+    {type:'url', label:'Short Answer'},
+    {type:'number', label:'Short Answer'},
+    {type:'select', label:'select'}
+
+]
