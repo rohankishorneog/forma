8b432f83669063514e8416bd89aced35be10435e
rohankishorneog <rkn2601@gmail.com>
2024-12-07 16:55:57 +0530

    cleaning up and setting the initial layout

---
+++ app/(root)/(Home)/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <Link href={"/create-form"}>
+        <button className="bg-green-500 text-white p-2 rounded-lg ">
+          navigate to create
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default page;
