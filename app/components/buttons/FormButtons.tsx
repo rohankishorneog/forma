c7d588ad2a0e44f19ef11174e26545b6e7a71f81
rohankishorneog <rkn2601@gmail.com>
2024-12-09 11:09:58 +0530

    created page to show all the forms

---
+++ app/components/buttons/FormButtons.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { deleteForm } from "@/lib/actions/forms.actions";
+import Link from "next/link";
+import React from "react";
+
+interface FormButtonsProps {
+  formId: string;
+}
+
+const FormButtons = ({ formId }: FormButtonsProps) => {
+  const handleDelete = async (id: string) => {
+    await deleteForm(id);
+  };
+
+  return (
+    <div className="flex gap-7 items-center">
+      <Link href={`/forms/${formId}/responses`}>
+        <button className="  px-4 py-2 rounded-xl border border-green-500">
+          Responses
+        </button>
+      </Link>
+      <div className="flex flex-col gap-2">
+        <Link href={`/forms/${formId}/edit`}>
+          <button className="bg-yellow-500 text-white px-3 py-1 rounded-md">
+            E
+          </button>
+        </Link>
+
+        <button
+          className="bg-red-500 text-white px-3 py-1 rounded-md"
+          onClick={() => handleDelete(formId)}
+        >
+          D
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FormButtons;
